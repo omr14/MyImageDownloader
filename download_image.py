dc3cfc68c4cb670e8e576ee8a4ebbd68f63a1d65
村石 辰徳 <muraishi.tatsunori@ad-hzm.co.jp>
2019-01-15 16:19:46 +0900

    first commit

---
+++ download_image.py
@@ -0,0 +1,62 @@
+import requests
+import datetime
+import json
+import os
+
+def download_image(url, auth=None):
+    """
+    urlに存在する画像を取得する
+    arguments
+        url: アクセス先URL
+        auth: 認証情報タプル('user', 'password')
+    return
+        画像のバイナリデータ
+    """
+    
+    # リクエストを送る
+    if auth:
+        res = requests.get(url, auth=auth)
+    else:
+        res = requests.get(url)
+    
+    # 失敗した場合を検出
+    if res.status_code != 200:
+        raise RuntimeError("HTTP Request Returns StatusCode " + str(res.status_code))
+    elif 'image' not in res.headers["content-type"]:
+        raise RuntimeError("HTTP Request Returns Bad Content-Type " + res.headers["content-type"])
+    
+    return res.content
+
+
+def save_binary(binary, path):
+    """
+    バイナリデータの画像を保存する
+    """
+    with open(path, "wb") as imgfile:
+        imgfile.write(binary)
+
+
+def make_filepath(root_dir):
+    """
+    ファイルの保存先を返す、root_dirで保存先のディレクトリを指定する
+    """
+    if not os.path.exists(root_dir):
+        os.mkdir(root_dir)
+
+    today = datetime.datetime.today()
+    path = root_dir + today.strftime("/%Y%m%d%H%M.png")
+    return path
+
+if __name__ == "__main__":
+
+    _f = open("./config.json")
+    config = json.load(_f)    
+    _f.close()
+
+    auth = (config["auth"]["user"], config["auth"]["path"])
+    binary = download_image(config["url"], auth)
+
+    image_root = config["directory"]
+    path = make_filepath(image_root)
+
+    save_binary(binary, path)
